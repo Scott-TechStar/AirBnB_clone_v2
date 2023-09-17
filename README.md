63d8550c32588b2dd390952e5d7cff00fd1eddff
John Mwadime <johnniekips@gmail.com>
2023-09-17 03:35:48 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+<p align="center">
+  <img src="https://github.com/Scott-TechStar/AirBnB_clone/blob/main/images/hbnb.png" alt="hBnB logo">
+</p>
+
+<h1 align="center">AirBnB Clone Project</h1>
+
+---
+
+## Description :house:
+
+Airbnb Clone Project is a complete web application, integrates database storage, 
+a back-end API, and front-end interfacing in a clone of AirBnB.
+
+## Currectly Implemented:
+  * The Console
+  * Webstatic-Frontend
---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Test for storage
+"""
+import os
+import sys
+import unittest
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.abspath(os.path.join(current_dir, "..","..",".."))
+sys.path.append(project_root)
+                               
+from datetime import datetime
+from time import sleep
+import json
+from models.engine.file_storage import FileStorage
+
+
+class test_fileStorage(unittest.TestCase):
+    """Test FileStorage Class"""
+    def test_instances(self):
+        """chequeamos instantation"""
+        obj = FileStorage()
+        self.assertIsInstance(obj, FileStorage)
+
+    def test_docs(self):
+        """Test docstrings"""
+        self.assertIsNotNone(FileStorage.all)
+        self.assertIsNotNone(FileStorage.new)
+        self.assertIsNotNone(FileStorage.save)
+        self.assertIsNotNone(FileStorage.reload)
+
+    if __name__ == '__main__':
+        unittest.main()
