b8894489ced4e4b5c1ba2e8baa8f7c92d7bcb93e
John Mwadime <johnniekips@gmail.com>
2023-10-06 20:35:20 +0300

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,34 +1,18 @@
-#!/usr/bin/env bash                                                                                                                                                     
-# Sets up a web server for deployment of web_static. 
+#!/usr/bin/env bash
+# a Bash script that sets up your web servers for the deployment of web_static
 
-sudo apt-get update
-sudo apt-get install -y nginx
-
+apt-get update
+apt-get -y install nginx
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "Hello World!" > /data/web_static/releases/test/index.html
+echo '<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>' > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
-
-printf %s "server {                                                                                                                                                     
-     listen      80 default_server;                                                                                                                                     
-     listen      [::]:80 default_server;                                                                                                                                
-     add_header X-Served-By $HOSTNAME;                                                                                                                                  
-     root        /etc/nginx/html;                                                                                                                                       
-     index       index.html index.htm;                                                                                                                                  
-                                                                                                                                                                        
-     location /redirect_me {                                                                                                                                            
-        return 301 https://scott-techstar.github.io/;                                                                                                                   
-    }                                                                                                                                                                   
-                                                                                                                                                                        
-    error_page 404 /404.html;                                                                                                                                           
-    location /404 {                                                                                                                                                     
-      root /etc/nginx/html;                                                                                                                                             
-      internal;                                                                                                                                                         
-    }                                                                                                                                                                   
-}                                                                                                                                                                       
-" > /etc/nginx/sites-available/default
-
+chown -hR ubuntu:ubuntu /data/
+sed -i '51 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
 service nginx restart
